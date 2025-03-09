f1b98637a4787fa4785e103c4310f65b54fccb0e
QuangVinh1605 <nhv16052006@gmail.com>
2025-03-09 23:51:04 +0700

    Update ex7.js

--- session 23/ex7.js
+++ session 23/ex7.js
@@ -1,29 +1,39 @@
-let arr = [2, 4, 6, 1, 8, 9];
+let n = +prompt("Nhap so luong phan phan tu cua mang");
 
-if (arr.length < 2) {
-    console.log("Mảng không có đủ phần tử để tìm số lớn thứ hai");
+if (n <= 0) {
+    console.log("so luong phan tu trong mang phair lon hon 0");
 } else {
-    let max1 = arr[0];
-    let max2 = arr[1];
+    let arr = [];
 
-    if (max1 < max2) {
-        let temp = max1;
-        max1 = max2;
-        max2 = temp;
+    for (let i = 0; i < n; i++) {
+        arr[i] = +prompt(`nhap phan tu thu ${i + 1}:`);
     }
 
-    for (let i = 2; i < arr.length; i++) {
-        if (arr[i] > max1) {
-            max2 = max1;
-            max1 = arr[i];
-        } else if (arr[i] > max2 && arr[i] !== max1) {
-            max2 = arr[i];
-        }
-    }
-
-    if (max1 === max2) {
-        console.log("Không có số lớn thứ hai");
+    if (n < 2) {
+        console.log("mang co it hon 2 phan tu");
     } else {
-        console.log("Số lớn thứ hai là:", max2);
+        let max1 = arr[0];
+        let max2 = arr[1];
+
+        if (max1 < max2) {
+            let temp = max1;
+            max1 = max2;
+            max2 = temp;
+        }
+
+        for (let i = 2; i < n; i++) {
+            if (arr[i] > max1) {
+                max2 = max1;
+                max1 = arr[i];
+            } else if (arr[i] > max2 && arr[i] !== max1) {
+                max2 = arr[i];
+            }
+        }
+
+        if (max1 === max2) {
+            console.log("khong co so lon thu hai");
+        } else {
+            console.log("so lon nthu hai la:", max2);
+        }
     }
 }
